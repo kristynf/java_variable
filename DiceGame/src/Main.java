7c3076dce464ac642b8c11e49203a382f4c879b7
kristynf <thetruthandsoul@gmail.com>
2020-01-24 10:39:30 -0500

    add challenge

---
+++ DiceGame/src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        //variables
+        int dice1;
+        int dice2;
+
+
+        //Call the welcome method
+        welcome();
+
+        //fetch random numbers
+
+
+        /*
+         * **************************************************************
+         *welcome method
+         *welcome user
+         *no parameters
+         *no return
+         ****************************************************************
+         */
+    }
+    public static void welcome() {
+        System.out.println("Welcome to a Lucky (for me) Dice Game! \nFEELING LUCKY?!? Hope you brought lots of CASH!");{
+        }
+
+        int dice1=(int)(Math.random()*6+1);
+        int dice2=(int)(Math.random()*6+1);
+        int sum= dice1 + dice2;
+        int[] values = new int[];  // Holds the highest values
+
+        System.out.println("Roll: total = " + sum);
+
+        public int highestValue() {
+            int sum = 0;
+            for (int i = 0;i < values.length;i++) {
+                sum = ((sum + values[i]));
+            }
+            return sum;
+        }
+        while(sum <100){
+         String
+
+        if (dice1==1 && dice2==1){
+            System.out.println("SNAKE EYES!" + sum + 25); }
+        else if(dice1==1 && dice2==2) {
+            System.out.println("You Lose!"); }
+            break;
+        else if(dice1==1 && dice2==3) {
+            System.out.println("You Lose!"); }
+            break;
+        else if(dice1==1 && dice2==4){
+            System.out.println("You Lose!"); }
+            break;
+        else if(dice1==1 && dice2==5) {
+            System.out.println("You Lose!"); }
+            break;
+        else if(dice1==1 && dice2==6) {
+            System.out.println("You Lose!"); }
+            break;
+
+
+
+        }
+            if(sum==4 ||sum==5 ||sum==6 ||sum==8 ||sum==9 ||sum==10)
+                dice1=(int)(Math.random()*6+1);
+            dice2=(int)(Math.random()*6+1);}
+        int roll2 = dice1 + dice2;}
+    System.out.print("You rolled "+roll2+"  ");{
+        while (roll2 !=7){
+            if (roll == roll2);{
+                System.out.println("You Win !");
+                break;
+            }else{
+
+            }
+        }}
